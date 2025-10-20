1113dd9086cb2abe07344b3e68a8e61774933292
maksluczak <maksluczak99@gmail.com>
2025-10-20 23:07:13 +0200

    add footer

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="py-4 px-4 sm:px-6 lg:px-8 border-t">
+            <div className="max-w-7xl mx-auto text-center text-xs">
+            <p>&copy; 2025 Nauka Pływania</p>
+            </div>
+        </footer> 
+    );
+}
